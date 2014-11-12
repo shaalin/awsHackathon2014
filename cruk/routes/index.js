7d18229c767884c2a714745d95a3ba4f1e2726f8
Malik Ahmed <maahmed@mdsol.com>
2014-11-11 16:36:33 -0800

    Use CDN bootstrap an add payment route

--- cruk/routes/index.js
+++ cruk/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET payment page. */
+router.get('/payment', function(req, res) {
+  res.render('payment', { title: 'Payment' });
+});
+
 module.exports = router;
